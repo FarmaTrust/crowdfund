ad18df3bf076ddb567093d08b14e813bb2d1c9c0
Michael Barbarelli <mbarbarelli@gmail.com>
2018-02-15 00:51:46 +0000

    Removal
    Removal of outdated FarmaTrust crowdsale contracts.

--- FTC/contracts/TokenInterface.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.17;
-
-contract TokenInterface {
-    function balanceOf(address _owner) public view returns (uint);
-    function transfer(address _to, uint _value) public returns (bool);
-    function unpause() public;
-}
